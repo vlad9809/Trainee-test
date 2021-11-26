dbefcf7d7221908405d9b11f79beb8a5b8f94ae9
Vlad <85299465+vlad9809@users.noreply.github.com>
2021-11-26 20:14:36 +0200

    Add files via upload
    version 3 without mobile

--- srcript.js
+++ srcript.js
@@ -9,9 +9,17 @@ let item = document.querySelector(".item")
 let btnThree = document.querySelector(".btnThree")
 let pages = document.querySelector(".pages")
 
+let signIn = document.querySelector(".signIn")
 let signInText = document.querySelector(".signInText")
-let regist = document.querySelector(".regist")
+let separator = document.querySelector(".separator")
+let regist = document.querySelector(".registText")
 
+let basket = document.querySelector(".basketText")
+
+let indicator = document.querySelector(".indicator")
+let goods = 49
+
+indicator.classList.remove("logIn")
 
 function logIn() {
     welcomeLine.classList.add("logIn")
@@ -23,9 +31,56 @@ function logIn() {
     btnThree.classList.add("logIn")
     pages.classList.add("logIn")
     signInText.classList.add("logIn")
+    separator.classList.add("logIn")
     regist.classList.add("logIn")
+    basket.classList.add("logIn")
+    
 }
 
 signInText.onclick = function() {
     logIn()
+    singIn()
 }
+
+regist.onclick = function() {
+    logIn()
+}
+
+function logOut() {
+    welcomeLine.classList.remove("logIn")
+    headerState.classList.remove("logIn")
+    topHalf.classList.remove("logIn")
+    aurora.classList.remove("logIn")
+    searchWindow.classList.remove("logIn")
+    item.classList.remove("logIn")
+    btnThree.classList.remove("logIn")
+    pages.classList.remove("logIn")
+    signInText.classList.remove("logIn")
+    signIn.classList.remove("logIn")
+    separator.classList.remove("logIn")
+    regist.classList.remove("logIn")
+    basket.classList.remove("logIn")
+    indicator.classList.remove("logIn")
+    separator.innerHTML = "|"
+    regist.innerHTML = "Register"
+    signInText.innerHTML = "Sign in"
+    signInText.onclick = function() {
+        logIn()
+        singIn()
+    }
+
+}
+
+function singIn() {
+    indicator.classList.add("logIn")
+    indicator.innerHTML = goods
+
+    separator.innerHTML = ""
+    regist.innerHTML = ""
+    signInText.innerHTML = "Hi, Angels!"
+    signIn.classList.add("logIn")
+    signInText.onclick = function() {
+        logOut()
+    }
+}
+
